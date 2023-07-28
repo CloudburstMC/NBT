33d724843dadc93bbecc381245388aaf0fc621a4
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2023-07-28 11:55:07 +0100

    Speed up and fix VarInt encoding

--- src/main/java/org/cloudburstmc/nbt/util/VarInts.java
+++ src/main/java/org/cloudburstmc/nbt/util/VarInts.java
@@ -8,61 +8,150 @@ public class VarInts {
     private VarInts() {
     }
 
-    public static void writeInt(DataOutput buffer, int integer) throws IOException {
-        encodeUnsigned(buffer, (integer << 1) ^ (integer >> 31));
+    public static void writeInt(DataOutput buffer, int value) throws IOException {
+        encode(buffer, ((value << 1) ^ (value >> 31)) & 0xFFFFFFFFL);
     }
 
     public static int readInt(DataInput buffer) throws IOException {
-        int n = (int) decodeUnsigned(buffer);
+        int n = (int) decode(buffer, 32);
         return (n >>> 1) ^ -(n & 1);
     }
 
-    public static void writeUnsignedInt(DataOutput buffer, long integer) throws IOException {
-        encodeUnsigned(buffer, integer);
+    public static void writeUnsignedInt(DataOutput buffer, long value) throws IOException {
+        encode(buffer, value & 0xFFFFFFFFL);
     }
 
     public static int readUnsignedInt(DataInput buffer) throws IOException {
-        return (int) decodeUnsigned(buffer);
+        return (int) decode(buffer, 32);
     }
 
-    public static void writeLong(DataOutput buffer, long longInteger) throws IOException {
-        encodeUnsigned(buffer, (longInteger << 1) ^ (longInteger >> 63));
+    public static void writeLong(DataOutput buffer, long value) throws IOException {
+        encode(buffer, (value << 1) ^ (value >> 63));
     }
 
     public static long readLong(DataInput buffer) throws IOException {
-        long n = decodeUnsigned(buffer);
+        long n = decode(buffer, 64);
         return (n >>> 1) ^ -(n & 1);
     }
 
-    public static void writeUnsignedLong(DataOutput buffer, long longInteger) throws IOException {
-        encodeUnsigned(buffer, longInteger);
+    public static void writeUnsignedLong(DataOutput buffer, long value) throws IOException {
+        encode(buffer, value);
     }
 
     public static long readUnsignedLong(DataInput buffer) throws IOException {
-        return decodeUnsigned(buffer);
+        return decode(buffer, 64);
     }
 
-    private static long decodeUnsigned(DataInput buffer) throws IOException {
+    // Based off of Andrew Steinborn's blog post:
+    // https://steinborn.me/posts/performance/how-fast-can-you-write-a-varint/
+    private static void encode(DataOutput out, long value) throws IOException {
+        // Peel the one and two byte count cases explicitly as they are the most common VarInt sizes
+        // that the server will write, to improve inlining.
+        if ((value & ~0x7FL) == 0) {
+            out.writeByte((byte) value);
+        } else if ((value & ~0x3FFFL) == 0) {
+            byte[] bytes = {
+                    (byte) (value & 0x7FL | 0x80L),
+                    (byte) (value >>> 7)
+            };
+            out.write(bytes);
+        } else {
+            encodeFull(out, value);
+        }
+    }
+
+    @SuppressWarnings({"DuplicatedCode"})
+    private static void encodeFull(DataOutput out, long value) throws IOException {
+        byte[] bytes;
+        if ((value & ~0x1FFFFFL) == 0) {
+            bytes = new byte[] {
+                    (byte) (value & 0x7FL | 0x80L),
+                    (byte) ((value >>> 7) & 0x7FL | 0x80L),
+                    (byte) (value >>> 14)
+            };
+        } else if ((value & ~0xFFFFFFFL) == 0) {
+            bytes = new byte[] {
+                    (byte) (value & 0x7FL | 0x80L),
+                    (byte) ((value >>> 7) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 14) & 0x7FL | 0x80L),
+                    (byte) (value >>> 21)
+            };
+        } else if ((value & ~0x7FFFFFFFFL) == 0) {
+            bytes = new byte[] {
+                    (byte) (value & 0x7FL | 0x80L),
+                    (byte) ((value >>> 7) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 14) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 21) & 0x7FL | 0x80L),
+                    (byte) (value >>> 28)
+            };
+        } else if ((value & ~0x3FFFFFFFFFFL) == 0) {
+            bytes = new byte[] {
+                    (byte) (value & 0x7FL | 0x80L),
+                    (byte) ((value >>> 7) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 14) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 21) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 28) & 0x7FL | 0x80L),
+                    (byte) (value >>> 35)
+            };
+        } else if ((value & ~0x1FFFFFFFFFFFFL) == 0) {
+            bytes = new byte[] {
+                    (byte) (value & 0x7FL | 0x80L),
+                    (byte) ((value >>> 7) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 14) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 21) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 28) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 35) & 0x7FL | 0x80L),
+                    (byte) (value >>> 42)
+            };
+        } else if ((value & ~0xFFFFFFFFFFFFFFL) == 0) {
+            bytes = new byte[] {
+                    (byte) (value & 0x7FL | 0x80L),
+                    (byte) ((value >>> 7) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 14) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 21) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 28) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 35) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 42) & 0x7FL | 0x80L),
+                    (byte) (value >>> 49)
+            };
+        } else if ((value & ~0x7FFFFFFFFFFFFFFFL) == 0) {
+            bytes = new byte[] {
+                    (byte) (value & 0x7FL | 0x80L),
+                    (byte) ((value >>> 7) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 14) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 21) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 28) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 35) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 42) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 49) & 0x7FL | 0x80L),
+                    (byte) (value >>> 56)
+            };
+        } else {
+            bytes = new byte[] {
+                    (byte) (value & 0x7FL | 0x80L),
+                    (byte) ((value >>> 7) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 14) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 21) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 28) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 35) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 42) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 49) & 0x7FL | 0x80L),
+                    (byte) ((value >>> 56) & 0x7FL | 0x80L),
+                    (byte) (value >>> 63)
+            };
+        }
+        out.write(bytes);
+    }
+
+    private static long decode(DataInput buffer, int maxBits) throws IOException {
         long result = 0;
-        for (int shift = 0; shift < 64; shift += 7) {
+        for (int shift = 0; shift < maxBits; shift += 7) {
             final byte b = buffer.readByte();
-            result |= (long) (b & 0x7F) << shift;
+            result |= (b & 0x7FL) << shift;
             if ((b & 0x80) == 0) {
                 return result;
             }
         }
-        throw new ArithmeticException("Varint was too large");
-    }
-
-    private static void encodeUnsigned(DataOutput buffer, long value) throws IOException {
-        while (true) {
-            if ((value & ~0x7FL) == 0) {
-                buffer.writeByte((int) value);
-                return;
-            } else {
-                buffer.writeByte((byte) (((int) value & 0x7F) | 0x80));
-                value >>>= 7;
-            }
-        }
+        throw new ArithmeticException("VarInt was too large");
     }
 }
